f5db77e108d54d8b58ec00baf09ef4d64211fa53
Jose Ferreira <jferreira590@gmail.com>
2020-05-11 19:14:33 -0400

    Creating Rails API and scaffolding users.

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  resources :users
+end
