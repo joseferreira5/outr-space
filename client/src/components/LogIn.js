ac4657a74855dfd3fac4e57355da333e012aec64
Jose Ferreira <jferreira590@gmail.com>
2020-05-17 10:47:48 -0400

    Refactored login page and moved styled components to shared.

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,51 +1,16 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import styled from 'styled-components';
 
 import Button from './shared/Button';
+import {
+  Container,
+  Form,
+  Heading,
+  Label,
+  Input,
+  Image,
+} from './shared/LoginSignup';
 import loginImage from '../assets/loginImage.jpg';
-
-const Section = styled.section`
-  display: grid;
-  grid-template-columns: 70% 1fr;
-  max-width: 700px;
-  min-height: 500px;
-  background-color: ${props => props.theme.lightShade};
-  margin: 0 auto;
-  border-radius: 0.3em;
-  margin-top: 10em;
-`;
-
-const Form = styled.form`
-  align-self: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  height: 15em;
-  padding-left: 1em;
-`;
-
-const Heading = styled.h2`
-  font-size: 2rem;
-`;
-
-const Label = styled.label`
-  font-size: 1.2rem;
-`;
-
-const Input = styled.input`
-  border-radius: 0.3em;
-  max-width: 70%;
-  font-family: 'Roboto';
-  font-size: 1rem;
-  padding: 0.2em;
-`;
-
-const Image = styled.div`
-  background-image: url(${loginImage});
-  background-position: center;
-`;
 
 export default function LogIn({ handleLogin }) {
   const [user, setUser] = useState({
@@ -69,7 +34,11 @@ export default function LogIn({ handleLogin }) {
   };
 
   return (
-    <Section>
+    <Container
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <Form onSubmit={handleSubmit}>
         <Heading>Log In</Heading>
         <Label htmlFor="username">
@@ -97,7 +66,7 @@ export default function LogIn({ handleLogin }) {
           Don't have an account? <Link to="/signup">Sign up here.</Link>{' '}
         </p>
       </Form>
-      <Image />
-    </Section>
+      <Image image={loginImage} />
+    </Container>
   );
 }
---
+++ client/src/components/shared/LoginSignup.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const Container = styled(motion.div)`
+  display: grid;
+  grid-template-columns: 70% 1fr;
+  max-width: 700px;
+  min-height: 500px;
+  background-color: ${props => props.theme.lightShade};
+  margin: 0 auto;
+  border-radius: 0.3em;
+  margin-top: 10em;
+`;
+
+export const Form = styled.form`
+  align-self: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 15em;
+  padding-left: 1em;
+`;
+
+export const Heading = styled.h2`
+  font-size: 2rem;
+`;
+
+export const Label = styled.label`
+  font-size: 1.2rem;
+`;
+
+export const Input = styled.input`
+  border-radius: 0.3em;
+  max-width: 70%;
+  font-family: 'Roboto';
+  font-size: 1rem;
+  padding: 0.2em;
+`;
+
+export const Image = styled.div`
+  background-image: url(${props => props.image});
+  background-position: center;
+`;
